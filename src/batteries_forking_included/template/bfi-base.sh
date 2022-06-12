70b7ad9afd0a679ba6a6d416b23344dfa6671b6d
Marximus Maximus <marximus@marximus.com>
2022-06-12 16:15:19 -0700

    switch from HOME to GITHUB_WORKSPACE b/c HOME isn't accessible as a job var

--- src/batteries_forking_included/template/bfi-base.sh
+++ src/batteries_forking_included/template/bfi-base.sh
@@ -810,9 +810,9 @@ create_my_tempdir() {
 
         if [ "${CI}" = true ]; then
             if [ "${GITHUB_ACTIONS}" = true ]; then
-                the_tempdir="${HOME}/bfi_temp/${GITHUB_ACTION}"; export CONDA_INSTALL_PATH
+                the_tempdir="${GITHUB_WORKSPACE}/bfi_temp/${GITHUB_ACTION}"; export CONDA_INSTALL_PATH
             else
-                the_tempdir="${HOME}/bfi_temp"
+                the_tempdir="${HOME}/bfi_temp/$(get_datetime_stamp_filename_formatted)"
             fi
         else
             the_tempdir=$(mktemp -d -t "$(get_my_real_basename)-$(get_datetime_stamp_filename_formatted).XXXXXXX")
