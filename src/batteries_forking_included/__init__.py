54b65b07867508257198d3e275d26ca84b3d9bce
Marximus Maximus <marximus@marximus.com>
2022-06-30 23:06:03 -0700

    allow implicit re-exports

--- src/batteries_forking_included/__init__.py
+++ src/batteries_forking_included/__init__.py
@@ -2,18 +2,4 @@
 batteries-forking-included python wrapper
 """
 
-from .__impl import (
-    bfi_bootstrap,
-    bfi_init,
-    bfi_run,
-    bfi_update,
-    BFI_VERSION,
-)
-
-__all__ = [
-    "bfi_bootstrap",
-    "bfi_init",
-    "bfi_run",
-    "bfi_update",
-    "BFI_VERSION",
-]
+from .__impl import *  # noqa: F401,F403
